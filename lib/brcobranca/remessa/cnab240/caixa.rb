cad641d5ea52ca8777368e40d89093bfa39be2ea
Maximiliano Brizolla <maxbrizolla@gmail.com>
2020-03-29 21:43:09 -0300

    Ajustado data correta nos campos de multa e mora

--- lib/brcobranca/remessa/cnab240/caixa.rb
+++ lib/brcobranca/remessa/cnab240/caixa.rb
@@ -138,7 +138,7 @@ module Brcobranca
 
         def data_multa(pagamento)
           return ''.rjust(8, '0') if pagamento.codigo_multa == '0'
-          pagamento.data_multa.strftime('%d%m%y')
+          pagamento.data_multa.strftime('%d%m%y').strftime('%d%m%Y')
           # if pagamento.data_multa == '000000'
           #   data_multa = pagamento.data_vencimento + 1
           #   data_multa.strftime('%d%m%Y')
